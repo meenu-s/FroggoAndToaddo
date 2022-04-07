f98281ed28faa43def92547ffca671cdc963fde2
Jenny Zhao <jennywuzhao@gmail.com>
2022-04-07 16:53:18 -0400

    Add scenes and change scene script

---
+++ Assets/Scripts/SwtichScreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwtichScreen : MonoBehaviour
+{
+    public void HomeScene() {
+        SceneManager.LoadScene("HomeScene");
+    }
+
+    public void HowToScene() {
+        SceneManager.LoadScene("HowToScene");
+    }
+
+    public void LevelScene() {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
