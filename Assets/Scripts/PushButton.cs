a188c3d3ed12d8196ae50f718f8f4edca6428376
Meenakshi Singh <mrsingh0306@gmail.com>
2022-04-19 16:51:25 -0400

    Moving platform control

--- Assets/Scripts/PushButton.cs
+++ Assets/Scripts/PushButton.cs
@@ -4,57 +4,33 @@ using UnityEngine;
 
 public class PushButton : MonoBehaviour
 {
-    public GameObject platform;
     // Start is called before the first frame update
-    public float currentHeight = -1.5f; //initial platform height
-    public float lowPlatformHeight = -1.5f;
-    public float highPlatformHeight = 0f;
-    private float currentPlatformSpeed = 0f;
-    public float platformSpeed = 0.1f;
-
+    public bool isPressed;
+    public float goalHeight = -1.5f;
 
     void Start()
     {
-        platform = GameObject.FindWithTag("platform");
-        platform.transform.position = new Vector3(platform.transform.position.x, currentHeight, 0);
-
+        isPressed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentHeight < highPlatformHeight && currentHeight > lowPlatformHeight)
-        {
-            currentHeight += currentPlatformSpeed;
-        }
-        else if (currentHeight < lowPlatformHeight)
-        {
-            currentHeight = lowPlatformHeight;
-        }
-        else if (currentHeight > highPlatformHeight)
-        {
-            currentHeight = highPlatformHeight;
-        }
-        else
-        {
-            currentPlatformSpeed = 0f;
-        }
-        platform.transform.position = new Vector3(platform.transform.position.x, currentHeight, 0);
+       
     }
 
-    void OnCollisionStay2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "froggo" || collision.gameObject.tag == "toaddo")
         {
-            currentPlatformSpeed = platformSpeed;
-            currentHeight += currentPlatformSpeed;
+            isPressed = true;
+            print("collided");
         }
     }   
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        currentPlatformSpeed = -platformSpeed;
-        currentHeight += currentPlatformSpeed;
+        isPressed = false;
     }
 }
 
