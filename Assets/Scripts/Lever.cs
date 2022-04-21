bb7a9b0d6d81bbe7789851bd06889ffae3bfd2a4
Meenakshi Singh <mrsingh0306@gmail.com>
2022-04-20 23:00:21 -0400

    Unfinished puzzle 4

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isPressed;
+    public float goalPosition = -1.5f;
+
+    void Start()
+    {
+        isPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "froggo" || collision.gameObject.tag == "toaddo")
+        {
+            isPressed = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isPressed = false;
+    }
+}
+
