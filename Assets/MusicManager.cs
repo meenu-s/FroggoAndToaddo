5510c383661d4867653b29e8d1968963eadf1943
Jenny Zhao <jennywuzhao@gmail.com>
2022-04-28 16:33:45 -0400

    Update first level

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour 
+{
+    private static MusicManager _instance;
+    public AudioSource audioData;
+ 
+    public static MusicManager instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<MusicManager>();
+                // gameObject.Play();
+                //Tell unity not to destroy this object when loading a new scene!
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+ 
+            return _instance;
+        }
+    }
+ 
+    void Awake() 
+    {
+        if(_instance == null)
+        {
+            //If I am the first instance, make me the Singleton
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            //If a Singleton already exists and you find
+            //another reference in scene, destroy it!
+            if(this != _instance)
+                Destroy(this.gameObject);
+        }
+    }
+ 
+    // private static void Play()
+    // {
+    //     audioData = GetComponent<AudioSource>();
+    //     audioData.loop = true;
+    //     audioData.Play(0);
+    //     Debug.Log("started");
+    // }
+}
